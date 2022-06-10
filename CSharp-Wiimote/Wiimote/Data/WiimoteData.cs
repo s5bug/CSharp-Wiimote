fd67f89579a1923f04830f09d0fffe28a53f7692
Aly <me@s5.pm>
2022-06-10 12:14:17 -0700

    Strip out Unity components

---
+++ CSharp-Wiimote/Wiimote/Data/WiimoteData.cs
@@ -0,0 +1,18 @@
+﻿namespace Wiimote.Data;
+
+public abstract class WiimoteData
+{
+    protected global::Wiimote.Wiimote Owner;
+
+    public WiimoteData(global::Wiimote.Wiimote Owner)
+    {
+        this.Owner = Owner;
+    }
+
+    /// \brief Interprets raw byte data reported by the Wii Remote.  The format of the actual bytes
+    ///        passed to this depends on the Wii Remote's current data report mode and the type
+    ///        of data being passed.
+    /// \sa Wiimote::ReadWiimoteData()
+    public abstract bool InterpretData(byte[] data);
+        
+}
